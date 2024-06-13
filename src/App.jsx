8fc342956ede195e4ec45578b86312099861c277
Chu-rill <churchillrimamskep@gmail.com>
2024-06-13 21:07:21 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className=" bg-blue-500 text-green-500 text-5xl text-center">
+        hello
+      </h1>
+    </>
+  );
+}
+
+export default App;
