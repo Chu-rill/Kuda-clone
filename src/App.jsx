7152cd0f7835532bb9db9993540b3861496e5b16
Chu-rill <churchillrimamskep@gmail.com>
2024-06-15 18:51:08 +0100

    i will soon give up

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import {
 } from "../src/components/ui/navigation-menu";
 import Header from "./components/Header";
 import Section1 from "./components/Section1";
-// import "@/style/style.css";
-// import "@/style/main.css";
+import "@/style/style.css";
+import "@/style/main.css";
 // import "@/style/second.css";
-import "@/style/root.css";
+// import "@/style/root.css";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
