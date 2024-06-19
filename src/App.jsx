623c2279014f3361f3eab2af27dde7a0dceb73d2
Chu-rill <churchillrimamskep@gmail.com>
2024-06-19 15:13:19 +0100

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,17 @@ import {
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route index path="/main" element={<Main />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/personal" element={<Personal />} />
-          <Route path="/business" element={<Business />} />
-          <Route path="/company" element={<Company />} />
-          <Route path="/help" element={<Help />} />
-        </Routes>
-      </Router>
-      {/* <Help /> */}
-      {/* <Company /> */}
-      {/* <Business /> */}
-      {/* <Personal /> */}
-      {/* <Menu /> */}
-      {/* <Main /> */}
-    </>
+    <Router>
+      <Routes>
+        <Route index element={<Main />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/personal" element={<Personal />} />
+        <Route path="/business" element={<Business />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
